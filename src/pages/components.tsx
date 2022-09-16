50876c6b696ca34d88097e75c8a6ac6ce2025c8a
nirel123 <nirel.gershwind@mirafinance.io>
2022-09-16 17:58:04 +0200

    more fixing merge

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -163,13 +163,13 @@ export const IndexModalBody: React.FC<IndexModalBodyProps> = ({
   const [referralReward, setReferralReward ] =useState<number>(0);
 
   const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
+    { name: "APT", value: 400 },
+    { name: "ETH", value: 300 },
+    { name: "BTC", value: 300 },
+    { name: "DOT", value: 200 },
   ];
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
+  const COLORS = ["#97acd0", "#5c87bf", "#4a7ab2", "#4470a5", "#3d6595", "#345882"]
+  
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({
     cx,
