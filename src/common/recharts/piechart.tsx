481f9bfd53e57cd5b6517712a659011ae5a2e99e
super-dev0728 <djtonyregent@gmail.com>
2022-10-05 23:35:10 -0400

    pie chart hover animation update

--- src/common/recharts/piechart.tsx
+++ src/common/recharts/piechart.tsx
@@ -4,16 +4,20 @@ import { Sector } from 'recharts';
 export const renderActiveShape = (props) => {
     const { cx, cy, innerRadius, outerRadius, startAngle, endAngle,
         fill } = props;
+    let _cx, _cy = 0;
+
+    _cx = cx + Math.cos((startAngle + endAngle) / 2 * Math.PI / 180) * 5;
+    _cy = cy + Math.sin((startAngle + endAngle) / 2 * Math.PI / 180) * (-5);
 
     return (
         <g>
             <Sector
-                cx={cx}
-                cy={cy}
+                cx={_cx}
+                cy={_cy}
                 innerRadius={innerRadius}
                 outerRadius={outerRadius * 1.07}
-                startAngle={startAngle + 2}
-                endAngle={endAngle - 2}
+                startAngle={startAngle}
+                endAngle={endAngle}
                 fill={fill}
             />
         </g>
