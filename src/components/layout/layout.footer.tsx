a1803198c5841e075400bcd97b1e763004f43fc1
0xPhoenix10 <jackson.dev.0820@gmail.com>
2022-10-11 08:36:58 +0900

    drag and drop button

--- src/components/layout/layout.footer.tsx
+++ src/components/layout/layout.footer.tsx
@@ -14,12 +14,75 @@ import {
   CoinIcon,
 } from "components/icons";
 import { useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styledComponents from "styled-components";
+
+import { styled, alpha } from '@mui/material/styles';
+import Button, { ButtonProps } from '@mui/material/Button';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 interface IItem {
   id: string,
   content: React.ReactElement,
 }
+const StyledButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  background: 'none',
+  boxShadow: '-5px -3px 10px 0px #fff1, -5px 5px 10px 0px #fff1, 5px 3px 10px 0px #0006',
+  borderRadius: "10px 10px 0px 0px",
+  border: '0px',
+  borderBottom: "none",
+  color: "#fff8",
+
+  '&:hover': {
+    background: 'none',
+    boxShadow: '-5px -3px 10px 0px #fff1, -5px 5px 10px 0px #fff1, 5px 3px 10px 0px #0006',
+    color: '#fff'
+  },
+}));
+const StyledMenu = styled((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
 const LayoutFooter = () => {
   const location = useLocation();
@@ -51,6 +114,15 @@ const LayoutFooter = () => {
     },
   ]);
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const openMoreMenu = Boolean(anchorEl);
+  const handleMoreMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMoreMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   // a little function to help us with reordering the result
   const reorder = (list, startIndex, endIndex) => {
     const result:IItem[] = Array.from(list);
@@ -62,8 +134,7 @@ const LayoutFooter = () => {
   const getListStyle = isDraggingOver => ({
     // background: isDraggingOver ? 'lightblue' : 'inherit',
     display: 'flex',
-    // padding: grid,
-    overflow: 'auto',
+    padding: '0px 15px',
   });
   const getItemStyle = (isDragging, draggableStyle) => ({
     // some basic styles to make the items look a bit nicer
@@ -99,6 +170,7 @@ const LayoutFooter = () => {
       p={"15px"}
       pb={"0px"}
       borderTop={"1px solid #333334"}
+      overflow={"auto"}
     >
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable" direction="horizontal">
@@ -131,11 +203,51 @@ const LayoutFooter = () => {
           )}
         </Droppable>
       </DragDropContext>
-      
+      <Flex gridGap={"25px"} pt={"5px"} ml={"10px"}>
+        <StyledButton
+          id="demo-customized-button"
+          aria-controls={openMoreMenu ? 'demo-customized-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={openMoreMenu ? 'true' : undefined}
+          variant="contained"
+          disableElevation
+          onClick={handleMoreMenuClick}
+          endIcon={<KeyboardArrowUpIcon />}
+        >
+          More
+        </StyledButton>
+        <StyledMenu
+          id="demo-customized-menu"
+          MenuListProps={{
+            'aria-labelledby': 'demo-customized-button',
+          }}
+          anchorEl={anchorEl}
+          open={openMoreMenu}
+          onClose={handleMoreMenuClose}
+        >
+          <MenuItem disableRipple>
+            <AddIcon />
+            Edit
+          </MenuItem>
+          <MenuItem disableRipple>
+            <AddIcon />
+            Duplicate
+          </MenuItem>
+          <Divider sx={{ my: 0.5 }} textAlign="left">Added</Divider>
+          <MenuItem onClick={handleMoreMenuClose} disableRipple>
+            <RemoveIcon />
+            Archive
+          </MenuItem>
+          <MenuItem onClick={handleMoreMenuClose} disableRipple>
+            <RemoveIcon />
+            More
+          </MenuItem>
+        </StyledMenu>
+      </Flex>
     </Flex>
   );
 };
-const FooterBtnBase = styled(Box)`
+const FooterBtnBase = styledComponents(Box)`
   &:hover {
     color: white;
   }
